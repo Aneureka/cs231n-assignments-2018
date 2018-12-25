9ec121d64a7d9d99115caf6b28f6d1563cadc4f7
Hiki <aneureka2@gmail.com>
2018-12-25 17:33:03 +0800

    Add svm naive loss gradient

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -34,13 +34,19 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        # For the incorrect datapoint, it has been computed once;
+        dW[:, j] += X[i]
+        # while for the correct datapoint, it has been computed X(margin>0) times
+        dW[:, y[i]] += -X[i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
+  dW += 2 * reg * W
 
   #############################################################################
   # TODO:                                                                     #
@@ -50,7 +56,6 @@ def svm_loss_naive(W, X, y, reg):
   # loss is being computed. As a result you may need to modify some of the    #
   # code above to compute the gradient.                                       #
   #############################################################################
-
 
   return loss, dW
 
